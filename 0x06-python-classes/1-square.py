ef2dfb7534f6c48bebac68c97d8a119894f67807
Linda <lchebet0@gmail.com>
2024-10-15 08:11:51 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,10 +6,10 @@
 class Square:
     """Instantiation with size: private attribute with no value"""
     def __init__(self, size):
-        self.__size = size 
+        self.__size = size
 
     def get_size(self):
         return self.__size
-    
+
     def set_size(self, size):
         self.__size = size
