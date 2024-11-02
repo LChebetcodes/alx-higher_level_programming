25ffe9453bac7df95deb99242e0e17f44952da0b
Linda <lchebet0@gmail.com>
2024-11-02 08:12:58 -0700

    0-lookup.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 def is_kind_of_class(obj, a_class):
-    """
-    Check if an object is an instance of a class or a subclass thereof.
+    """ Check if an object is an instance of a class or a subclass thereof.
 
     Parameters:
     obj: The object to check.
