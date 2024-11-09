438397ccc8e2a007a88c2ab4e6db25c1f48e41a2
Linda <lchebet0@gmail.com>
2024-11-09 05:22:16 -0800

    4-print_square.py, tests/4-print_square.txt

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A simple module that that prints a square with the character #."""
+
+
+def print_square(size):
+    """Check if size is an integer"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    """Check if size is non-negative"""
+    if size < 0:
+        raise ValueError("size must be >= 0")
+        
+    """Print the square"""
+    for _ in range(size):
+        print("#" * size)
