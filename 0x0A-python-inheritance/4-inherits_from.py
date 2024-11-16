fa0f62b7ec7db3abb73f1314094ed9598fe9abd3
Linda <lchebet0@gmail.com>
2024-11-16 04:10:47 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,18 +5,20 @@ is an instance of a class that inherited (directly or indirectly) from a
 specified class, excluding instances of the class itself.
 """
 
+
 def inherits_from(obj, a_class):
     """
     Determines if an object is an instance of a class that inherited (directly
-    or indirectly) from the specified class. Excludes instances of the specified
-    class itself.
+    or indirectly) from the specified class.
+    Excludes instances of the specified class itself.
 
     Args:
         obj: The object to check.
         a_class: The class to match against.
 
     Returns:
-        True if obj is an instance of a subclass of a_class (but not a_class itself), False otherwise.
+        True if obj is an instance of a subclass of
+        a_class (but not a_class itself), False otherwise.
 
     Examples:
         >>> class A:
