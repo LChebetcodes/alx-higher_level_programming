1fe80fed693b501ea80a0482d827f82112d07b68
Linda <lchebet0@gmail.com>
2024-12-11 06:42:52 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,8 +2,10 @@
 """
 This module provides a function  that writes a string to a text file"""
 
+
 def append_write(filename="", text=""):
-    """Appends a string to a text file (UTF-8) and returns the number of characters added."""
+    """Appends a string to a text file
+    """
     with open(filename, "a", encoding="utf-8") as file:
         chars_written = file.write(text)
     return chars_written
